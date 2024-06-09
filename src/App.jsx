26ea99bcd7cf331a01b4986fad6b11be3fa5a192
tvxxd <tavdgiridzenika1@gmail.com>
2024-06-09 20:31:06 +0400

    add: sort | clearall

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,10 @@ export default function App() {
     );
   }
 
+  function handleClearAll() {
+    setBooks([]);
+  }
+
   return (
     <>
       <Logo />
@@ -29,6 +33,7 @@ export default function App() {
         onHandleToggleBook={handleToggleBook}
         onDeleteBook={handleDeleteBook}
         books={books}
+        handleClearAll={handleClearAll}
       />
       <Summary books={books} />
     </>
@@ -73,20 +78,42 @@ function Form({ onBookAdd }) {
     </form>
   );
 }
-function BookList({ books, onDeleteBook, onHandleToggleBook }) {
+function BookList({ books, onDeleteBook, onHandleToggleBook, handleClearAll }) {
+  const [sortBy, setSortBy] = useState("latest");
+
+  let sorted;
+
+  if (sortBy === "latest") sorted = books;
+  if (sortBy === "name")
+    sorted = books.slice().sort((a, b) => a.name.localeCompare(b.name));
+  if (sortBy === "quantity")
+    sorted = books.slice().sort((a, b) => b.quantity - a.quantity);
+  if (sortBy === "read")
+    sorted = books.slice().sort((a, b) => Number(a.read) - Number(b.read));
   return (
-    <div className="book-list">
-      <ul>
-        {books.map((book) => (
-          <Book
-            handleToggleBook={onHandleToggleBook}
-            handleDeleteBook={onDeleteBook}
-            key={book.id}
-            book={book}
-          />
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="book-list">
+        <ul>
+          {sorted.map((book) => (
+            <Book
+              handleToggleBook={onHandleToggleBook}
+              handleDeleteBook={onDeleteBook}
+              key={book.id}
+              book={book}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className="manage">
+        <select onChange={(e) => setSortBy(e.target.value)} value={sortBy}>
+          <option value="latest">Sort by latest</option>
+          <option value="name">Sort by name</option>
+          <option value="quantity">Sort by quantity</option>
+          <option value="read">Sort by read</option>
+        </select>
+        <button onClick={() => handleClearAll()}>Clear All</button>
+      </div>
+    </>
   );
 }
 function Book({ book, handleDeleteBook, handleToggleBook }) {
